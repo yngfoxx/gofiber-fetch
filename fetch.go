1472e3f51eee456dcc2376486f3705e14fd515d5
Stephen Osunrinde <47150226+yngfoxx@users.noreply.github.com>
2022-11-05 06:22:35 +0000

    first commit

---
+++ fetch.go
@@ -0,0 +1,101 @@
+package fetch
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+)
+
+type RequestHeader struct {
+	Method        string      `json:"Method,omitempty"`
+	Body          interface{} `json:"Body,omitempty"`
+	Header        []string    `json:"Header"`
+	Authorization string      `json:"Authorization,omitempty"`
+	Agent         *fiber.Agent
+}
+
+type Respnse struct {
+	Status int
+	Data   interface{}
+	Error  error
+}
+
+func Method(method string) *RequestHeader {
+	return &RequestHeader{
+		Method: method,
+	}
+}
+
+func (header *RequestHeader) SetAuthorization(auth string) *RequestHeader {
+	header.Authorization = auth
+	return header
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func (header *RequestHeader) FiberFetch(url string) *Respnse {
+
+	// FastHttp agent
+	agent := (map[bool]*fiber.Agent{
+		true:  header.Agent,
+		false: fiber.AcquireAgent(),
+	})[header.Agent != nil]
+
+	// Prepare request
+	req := agent.Request()
+	req.Header.SetMethod(header.Method)
+	req.SetRequestURI(url)
+
+	// body
+	if header.Body != nil && !stringInSlice(header.Method, []string{"GET", "HEAD"}) {
+		payload, _ := json.Marshal(header.Body)
+		agent.Body(payload)
+	}
+	// authorization
+	if header.Authorization != "" {
+		agent.Set("Authorization", header.Authorization)
+	}
+	// headers
+	if len(header.Header) > 0 {
+		for _, v := range header.Header {
+			h := strings.Split(v, "=")
+			if len(h) == 2 {
+				agent.Set(h[0], h[1])
+			}
+		}
+	}
+
+	// Execute
+	if err := agent.Parse(); err != nil {
+		return &Respnse{}
+	}
+
+	// Parse
+	code, body, errs := agent.Bytes()
+	if len(errs) > 0 {
+		var errStr string
+		for _, v := range errs {
+			errStr += v.Error() + "; "
+		}
+		if code == 0 {
+			code = 500
+		}
+		return &Respnse{Status: code, Error: errors.New(errStr)}
+	}
+
+	if code >= 400 && body != nil {
+		return &Respnse{Status: code, Error: fmt.Errorf("error: %s", string(body))}
+	}
+
+	return &Respnse{Status: code, Data: body}
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/yngfoxx/gofiber-fetch
+
+go 1.19
+
+require (
+	github.com/goccy/go-json v0.9.11
+	github.com/gofiber/fiber/v2 v2.39.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
